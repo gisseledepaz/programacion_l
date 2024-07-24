51f028bf61a60687c2f9f5b174884e6a7582775b
Gissele De Paz <ustr031124@ugb.edu.sv>
2024-07-24 10:16:06 -0600

    determinar de una serie de numeros

--- notas/notas/Program.cs
+++ notas/notas/Program.cs
@@ -10,8 +10,16 @@ namespace notas
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("vamos por un diez");
-
+            // Prioridad de los operdores aritmeticos.
+         //ejercicio obtener el promedio de una serie de numeros 
+         int[] serie = new int[] { 5, 4, 6, 8, 9 }; //32
+            int suma = 0;
+            foreach (int num in serie)
+            { suma = suma + num; }
+            decimal prom = suma / serie.Length;
+            Console.WriteLine("La suma es: {0}, el promedio {1}", suma, prom);
+            //Pausa.
+            Console.ReadLine();
         }
     }
 }
