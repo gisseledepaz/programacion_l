8bc619fe4fc24e3bb4bdd5dbf15b5907b4120750
Gissele Castro <MINEDUCYT@DESKTOP-EMGKJOF>
2024-07-24 07:22:08 -0600

    ha sido completado

---
+++ notas/notas/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace notas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("vamos por un diez");
+
+        }
+    }
+}
