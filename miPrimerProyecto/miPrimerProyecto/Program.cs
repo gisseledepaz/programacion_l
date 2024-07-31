c7752d0a77e0edf80ecd5261e38bce2b96ddf395
Gissele Castro <MINEDUCYT@DESKTOP-EMGKJOF>
2024-07-31 10:13:43 -0600

    suma de dos numeros introducidos por el usuario

---
+++ miPrimerProyecto/miPrimerProyecto/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace miPrimerProyecto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Num1:");
+            int num1 =
+            int.Parse(Console.ReadLine());
+            
+           
+            Console.Write("Num 2: ");
+            int num2 =
+            int.Parse(Console.ReadLine());
+
+             int respuesta = num1 + num2;
+            Console.WriteLine("La auma de {0} + {1} + {2}", num1, num2, respuesta);
+            //Pause.
+            Console.ReadLine();
+        }
+    }
+}
