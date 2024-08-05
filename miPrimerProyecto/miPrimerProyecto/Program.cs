8848e4a449ad0a59b5616dd5ac21344b8bdd4346
Gissele Castro <MINEDUCYT@DESKTOP-EMGKJOF>
2024-08-05 12:42:48 -0600

    pedir al usuario  la edad si es mayor que le diga bienvenida

--- miPrimerProyecto/miPrimerProyecto/Program.cs
+++ miPrimerProyecto/miPrimerProyecto/Program.cs
@@ -14,24 +14,29 @@ namespace miPrimerProyecto
             //ejerccio obtner el promedio de una serie de numeros
             int[] serie = new int[] { 5, 6, 8, 9 }; //32
             int sum = 0;
-            foreach (int num in serie){
+            foreach (int num in serie)
+            {
                 sum += num;
 
-            //estructuras de control
-            //1.if.ejercicio.pedir al usuio laedad si es mayr de dad que le diga bienenido 
-            Console.Write("Edad: ");
-            int edad = int.Parse(Console.ReadLine());
-             
-            if (edad <= 18){
-                Console.WriteLine("Bienvenido al mundo de las responsabiliddes ");
-            }else {
-                Console.WriteLine("Es un adolecente, disfruta de la vida.");
+                //estructuras de control
+                //1.if.ejercicio.pedir al usuario la edad si es mayar de edad que le diga bienenido 
+                Console.Write("Edad: ");
+                int edad = int.Parse(Console.ReadLine());
+
+                if (edad <= 18)
+                {
+                    Console.WriteLine("Bienvenido al mundo de las responsabiliddes ");
+                }
+                else
+                {
+                    Console.WriteLine("Es un adolecente, disfruta de la vida.");
+                }
+                decimal promedio = sum / serie.Length;
+                Console.WriteLine("La suma es: {0}, el promedio {1}", sum, promedio);
             }
-            decimal promedio = sum / serie.Length;
-            Console.WriteLine("La suma es: {0}, el promedio {1}", sum, promedio);
 
 
-            //Pausa.
+               //Pausa.
             Console.ReadLine ( ) ;
         }
     }
