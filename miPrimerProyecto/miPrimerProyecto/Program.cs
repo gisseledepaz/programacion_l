1cd9fa8f3e4283b6511e3027b8957b875ebee13d
Gissele Castro <MINEDUCYT@DESKTOP-EMGKJOF>
2024-08-05 11:49:58 -0600

    Tipos de datos en C#

--- miPrimerProyecto/miPrimerProyecto/Program.cs
+++ miPrimerProyecto/miPrimerProyecto/Program.cs
@@ -11,28 +11,26 @@ namespace miPrimerProyecto
     {
         static void Main(string[] args)
         {
-            //Prioridad de los operadores aritmeticos.
-            //ejercicios obtener el promedio de una serie de numeros
-            int[] serie = new int[] { 5, 4, 6, 8, 9 }; //32
-            int suma = 0;
-            foreach ( int num in serie) {
-                suma += num ;
-            }
-             decimal prom = suma / serie.Length;
-            Console.WriteLine("la suma es: {0}, el promedio es {1}", suma, prom);
-            Console.WriteLine("Num1 :");
-            int num1 = int.Parse(Console.ReadLine());
-            num1 = sbyte.Parse(Console.ReadLine());
+            Console.Write("Num1: ");
+            sbyte num1 = sbyte.Parse(Console.ReadLine());
+            Boolean comprobar = true;
+            Console.WriteLine("El valor es: {0}", comprobar);
 
-            Console.WriteLine("Num 2:");
-            int num2 = int.Parse(Console.ReadLine());
-            num2 = sbyte.Parse(Console.ReadLine());
+            Console.Write("num2: ");
+            sbyte num2 = 
+            sbyte.Parse(Console.ReadLine());
+            Console.Write("Nombre: ");
+            string nombre = Console.ReadLine();
 
-            int respuesta = num1 + num2;
-            Console.WriteLine("la suma de {0} + {1} = {2}", num1 + num2, respuesta);
-            
+            int suma = num1 + num2;
+
+            Console.WriteLine("La suma de {0} + {1} = {2}", num1, num2, suma);
+            Console.WriteLine("Hola {0} bienvenido", nombre);
+
+            DateTime fechaActual =  DateTime.Now;
+            Console.WriteLine("La fecha actual es: {0}", fechaActual);
             //Pausa.
-            Console.ReadLine();
+            Console.ReadLine ( ) ;
         }
     }
 }
