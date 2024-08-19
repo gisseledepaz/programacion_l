602413bf74e6ba657c83ca550e855dba39c0713e
Gissele Castro <MINEDUCYT@DESKTOP-EMGKJOF>
2024-08-19 17:14:13 -0600

    Uso de matriz bidimencional

--- miPrimerProyecto/miPrimerProyecto/Program.cs
+++ miPrimerProyecto/miPrimerProyecto/Program.cs
@@ -9,20 +9,32 @@ namespace miPrimerProyecto
 {
     class Program
     {
-        static void Main(string[] args) { 
-        
+        static void Main(string[] args)
+        {
+
             //Uso de matrices.
             int[] numeros = new int[] { 2, 8, 9, 10, 1, 3 };
-            Console.WriteLine("El numero de la posicion 3 es {0}", numeros[3]);
-            for (int i=0; i < numeros.Length; i++) {
+            for (int i = 0; i < numeros.Length; i++)
+            {
                 Console.WriteLine("Indice: {0}={1}", i, numeros[i]);
+                int[,] matriz = new int[,] {
+                    { 5, 4, 5, 2, 1 },
+                    { 1, 2, 8, 9, 3 }
+            };
+                for ( i = 0;i<2; i++)
+                {
+                    for (int j = 0; j < 5; j++)
+                    {
+                        Console.Write("{0} ", matriz[i, j]);
+                    }
+                    Console.WriteLine("\n");
+                }
+                //Pausa
+                Console.ReadKey();
             }
-            //Pausa
-            Console.ReadKey();
         }
     }
-}    
-
+}   
 
 
              
